d1d19654602a16dc3a89786fba7aac720e0e317a
bedux <bedulm@usi.ch>
2014-04-05 11:54:13 +0200

    Initial Commit

---
+++ testBullet/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  testBullet
+//
+//  Created by Marco Bedulli on 4/5/14.
+//  Copyright (c) 2014 Marco Bedulli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
